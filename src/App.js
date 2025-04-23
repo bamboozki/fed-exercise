2cfdcb345bb827c510667309bd2c0c9335627f1f
Bamboozki <jyruscarrasco@gmail.com>
2025-04-23 20:25:00 +0800

    init: created react app for project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">React App Ready</h1>
+    </div>
+  );
+}
+
+export default App;
